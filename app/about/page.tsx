c980465091095ee663b24bfa4bae2153cb47fcda
izzy <mista.rol@gmail.com>
2024-02-12 12:09:25 +0700

    boiler plate from v0

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold text-center my-8">About Us</h1>
+      <p className="text-lg text-gray-700">
+        This is the about page for our Next.js application. Here you can find information about our team, our mission, and the technologies we use.
+      </p>
+    </div>
+  );
+}
